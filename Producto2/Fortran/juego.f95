84787eaac42889b57e9aba208c38e7335d5f05f9
Martin ALejandro Paredes Sosa <mapsjr16@gmail.com>
2015-02-12 16:56:26 -0700

    Se lograron avances se compilo el fortran

---
+++ Producto2/Fortran/juego.f95
@@ -0,0 +1,21 @@
+program juego
+
+  write(*,*) 'Hola, es hora de jugar ADIVINA EL NUMERO. Primero piensa en un numero entre 1 y 10'
+  
+  call sleep(5)
+
+  Write(*,*) 'Multiplicalo por 9'
+
+  call sleep(5)
+
+  write(*,*) 'Si el numero es de dos digitos, sumalos entre si. Por ejemplo si es 25 -> 2+5=7'
+
+  call sleep(5)
+
+  write(*,*) 'El numero resultante sumale 4'
+
+  call sleep(10)
+
+  write(*,*) 'Bien. El resultado es 13'
+
+end program juego
