ef9e4946a6a8677f0cd521b099d7d53b9f14c573
Martin ALejandro Paredes Sosa <mapsjr16@gmail.com>
2015-02-12 17:07:52 -0700

    Se lograron avances se compilo el Python

---
+++ Producto2/Python/juego.py
@@ -0,0 +1,19 @@
+import time
+
+print "Hola, es hora de jugar ADIVINA EL NUMERO. Primero piensa en un numero entre 1 y 10"
+import time
+time.sleep(5)
+
+print "Multiplicalo por 9"
+import time
+time.sleep(5)
+
+print "Si el numero es de dos digitos, sumalos entre si. Por ejemplo si es 25 -> 2+5=7"
+import time
+time.sleep(5)
+
+print "El numero resultante sumale 4"
+import time
+time.sleep(10)
+
+print "Bien. El resultado es 13"
