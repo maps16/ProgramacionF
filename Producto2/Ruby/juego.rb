b523a835a0644d7c5eca4a3308f830e247888d86
Martin ALejandro Paredes Sosa <mapsjr16@gmail.com>
2015-02-12 17:12:43 -0700

    Se lograron avances se compilo el Ruby

---
+++ Producto2/Ruby/juego.rb
@@ -0,0 +1,16 @@
+  puts "Hola, es hora de jugar ADIVINA EL NUMERO. Primero piensa en un numero entre 1 y 10"
+  sleep(5)
+  
+  puts "Multiplicalo por 9"
+  
+  sleep(5)
+
+  puts "Si el numero es de dos digitos, sumalos entre si. Por ejemplo si es 25 -> 2+5=7"
+
+  sleep(5)
+
+  puts"El numero resultante sumale 4"
+  
+  sleep(10)
+
+  puts "Bien. El resultado es 13"
